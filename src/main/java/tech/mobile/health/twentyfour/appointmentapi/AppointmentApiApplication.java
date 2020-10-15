4e984bb0e02387a7c6afd74abed8555d05376b43
snchitovhoro <snchitovhoro@gmail.com>
2020-10-15 16:59:59 +0200

    Initial commit

---
+++ src/main/java/tech/mobile/health/twentyfour/appointmentapi/AppointmentApiApplication.java
@@ -0,0 +1,13 @@
+package tech.mobile.health.twentyfour.appointmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentApiApplication.class, args);
+    }
+
+}
